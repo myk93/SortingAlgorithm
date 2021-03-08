0dd69693d66858150f2138f6060304bab2274b00
David Boublil <davdsb@gmail.com>
2021-03-08 18:37:57 +0200

    data chart is now an item control

--- SortAlgorithm_UI/ChartView_VM.cs
+++ SortAlgorithm_UI/ChartView_VM.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Collections.ObjectModel;
-using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Threading;
 using Sorting_algorithm;
 
 namespace SortAlgorithm_UI
 {
-    class ViewModel : BaseViewModel
+    public class ChartView_VM : BaseViewModel
     {
-        // properties
         private ObservableCollection<int> _elementsList;
-        public ObservableCollection<int> ElementsList {
+        public ObservableCollection<int> ElementsList
+        {
             get { return _elementsList; }
             set { _elementsList = value; OnPropertyChanged(); }
 
         }
 
+        private int _elementListSize;
+
         public int ElementListSize
         {
-            get => ElementsList.Count;
+            get => _elementListSize;
             set
             {
-                ElementsList = new ObservableCollection<int>(Enumerable.Range(0, value));
+                _elementListSize = value;
+                ElementsList = new ObservableCollection<int>(Enumerable.Range(0, ElementListSize));
                 OnPropertyChanged();
             }
         }
@@ -48,16 +52,30 @@ namespace SortAlgorithm_UI
             Sorter.Shuffle();
         }
 
-        // Constructor
-        public ViewModel()
+        public ICommand SortCommand { get; set; }
+
+        public void Sort(object param)
+        {
+            if (Sorter.arr != ElementsList)
+                Sorter.arr = ElementsList;
+            Sorter.DoSort(Dispatcher.CurrentDispatcher);
+        }
+
+        public ChartView_VM()
         {
             ElementListSize = 100;
-            ElementsList = new ObservableCollection<int>(Enumerable.Range(0,ElementListSize));
+            ElementsList = new ObservableCollection<int>(Enumerable.Range(0, ElementListSize));
             Sorter = new BubbleSort(ElementsList);
 
             // commands implementation
             ShuffleCommand = new RelayCommand(Shuffle);
+            SortCommand = new RelayCommand(Sort);
         }
-        
+
+        public ChartView_VM(ObservableCollection<int> collection, BaseSort sorter)
+        {
+            ElementsList = collection;
+            Sorter = sorter;
+        }
     }
 }
