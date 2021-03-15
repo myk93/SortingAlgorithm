b3b18813b0b8193b711a4b616dc0686fea7af85b
Meni Kroizer <meni.kroizer@gmail.com>
2021-03-15 21:27:56 +0200

    quick sort operational

--- sortingAlgorithm1/quickSort.cs
+++ sortingAlgorithm1/quickSort.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Threading;
 
@@ -20,30 +21,34 @@ namespace sortingAlgorithm
         public override int DoSort(Dispatcher d = null, int pause = 0)
         {
 
-            Quick_Sort(0,arr.Count-1);
-            
+            Quick_Sort(0, arr.Count - 1, d,pause);
+
             return 1;
         }
-        private  void Quick_Sort(int left, int right)
+        private void Quick_Sort(int left, int right, Dispatcher d,int pause)
         {
-            if (left < right)
+            Task.Factory.StartNew(() =>
             {
-                int pivot = Partition( left, right);
+                if (left < right)
+                {
+                    int pivot = Partition(left, right, d,pause);
 
-                if (pivot > 1)
-                {
-                    Quick_Sort( left, pivot - 1);
+                    if (pivot > 1)
+                    {
+                        Quick_Sort(left, pivot - 1, d,pause);
+                    }
+                    if (pivot + 1 < right)
+                    {
+                        Quick_Sort(pivot + 1, right, d,pause);
+                    }
                 }
-                if (pivot + 1 < right)
-                {
-                    Quick_Sort( pivot + 1, right);
-                }
-            }
+            });
 
         }
 
-        private  int Partition( int left, int right)
+        private int Partition(int left, int right, Dispatcher d,int pause)
         {
+
             int pivot = arr[left];
             while (true)
             {
@@ -61,12 +66,8 @@ namespace sortingAlgorithm
                 if (left < right)
                 {
                     if (arr[left] == arr[right]) return right;
-
-                    int temp = arr[left];
-                    arr[left] = arr[right];
-                    arr[right] = temp;
-
-
+                    d.Invoke(() => Swap(left, right));
+                    Thread.Sleep(pause);
                 }
                 else
                 {
@@ -74,23 +75,6 @@ namespace sortingAlgorithm
                 }
             }
         }
-        private void quickSortAlgo( Dispatcher d, int left, int right)
-        {
-            Task.Factory.StartNew(() => {
-                if (IsSorted())
-                {
-                    return;
-                }
-                int pivot = arr[0];
-                int leftSide = arr[left];
-                int rightSide = arr[right];
 
-
-                quickSortAlgo(d,left+1,left - 1);
-               
-            });
-
-            throw new NotImplementedException();
-        }
     }
 }
